9d0f4aad0c94796d7ff29a4476e370a6698f9f5a
dingqiaokun <qiaokun.d@wingto.cn>
2021-01-15 18:11:02 +0800

    [mybatis-plus]-gengrator生成代码

---
+++ mybatisPlus/src/main/java/com/dqk/test/BaseDto.java
@@ -0,0 +1,24 @@
+package com.dqk.test;
+
+import java.io.Serializable;
+
+public class BaseDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	/** 新增时间 */
+	protected String createDate;
+	/** 更新时间 */
+	protected String updateDate;
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+}
