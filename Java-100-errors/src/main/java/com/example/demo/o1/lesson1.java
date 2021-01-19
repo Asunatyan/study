69b44152c7d14725336bebf8be755fadad96176a
tamakiako <tamakiako@164.com>
2021-01-19 22:14:57 +0800

    [java100error] map相关

--- Java-100-errors/src/main/java/com/example/demo/o1/lesson1.java
+++ Java-100-errors/src/main/java/com/example/demo/o1/lesson1.java
@@ -25,7 +25,7 @@ public class lesson1 {
 
 
     @GetMapping("wrong")
-    public Map wrong(@RequestParam("userId") Integer userId) {
+    public Map<String,String> wrong(@RequestParam("userId") Integer userId) {
         //设置用户信息之前先查询一次ThreadLocal中的用户信息
         String before  = Thread.currentThread().getName() + ":" + currentUser.get();
         //设置用户信息到ThreadLocal
@@ -34,7 +34,7 @@ public class lesson1 {
         String after  = Thread.currentThread().getName() + ":" + currentUser.get();
 
         //汇总输出两次查询结果
-        Map result = new HashMap();
+        Map<String,String> result = new HashMap<>();
         result.put("before", before);
         result.put("after", after);
         return result;
