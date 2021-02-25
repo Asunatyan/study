0c5b3bbca26aa92f9708e9b6037b6b860ddee49b
dingqiaokun <qiaokun.d@wingto.cn>
2021-02-25 09:48:59 +0800

    整理

--- spring-related/spring-security-oauth2/spring-security-oauth2-server/src/main/java/com/dqk/oauth2/server/mapper/TbUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dqk.oauth2.server.mapper;
-
-import com.dqk.oauth2.server.domain.TbUser;
-import tk.mybatis.mapper.MyMapper;
-
-public interface TbUserMapper extends MyMapper<TbUser> {
-}
--- spring-related/spring-security-oauth2/spring-security-oauth2-server/src/main/java/com/dqk/oauth2/server/mapper/TbUserRoleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dqk.oauth2.server.mapper;
-
-import com.dqk.oauth2.server.domain.TbUserRole;
-import tk.mybatis.mapper.MyMapper;
-
-public interface TbUserRoleMapper extends MyMapper<TbUserRole> {
-}
