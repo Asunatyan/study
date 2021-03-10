225bc63fbcc6fbd93424bbc0a38e84fa35ae25d1
dingqiaokun <qiaokun.d@wingto.cn>
2021-03-10 17:56:52 +0800

    原型模式

---
+++ time-geekbang/design-patterns/src/main/java/com/example/designpatterns/part05/prototype/SearchWord.java
@@ -0,0 +1,17 @@
+package com.example.designpatterns.part05.prototype;
+
+/**
+ * Description:
+ * date: 2021/3/10 17:19
+ *
+ * @author dqk
+ */
+public class SearchWord {
+    public long getLastUpdateTime() {
+        return 12323l;
+    }
+
+    public String getKeyword() {
+        return null;
+    }
+}
