0effa09fcfa5af580373e9101e27210c8477344c
dingqiaokun <12345678>
2021-06-21 12:59:33 +0800

    update

--- mybatisPlus/mybatis-demo/src/main/java/com/dqk/test/generator/CodeGenerator2.java
+++ mybatisPlus/mybatis-demo/src/main/java/com/dqk/test/generator/CodeGenerator2.java
@@ -23,25 +23,6 @@ import java.util.Scanner;
 public class CodeGenerator2 {
     private static final Logger logger = LoggerFactory.getLogger(AutoGenerator.class);
 
-    /**
-     * <p>
-     * 读取控制台内容
-     * </p>
-     */
-    public static String scanner(String tip) {
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder help = new StringBuilder();
-        help.append("请输入" + tip + "：");
-        System.out.println(help.toString());
-        if (scanner.hasNext()) {
-            String ipt = scanner.next();
-            if (StringUtils.isNotBlank(ipt)) {
-                return ipt;
-            }
-        }
-        throw new MybatisPlusException("请输入正确的" + tip + "！");
-    }
-
     public static void main(String[] args) {
         // 代码生成器
 //        AutoGenerator mpg = new MyAutoGenerator();
@@ -70,7 +51,7 @@ public class CodeGenerator2 {
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir(projectPath + "/mybatis-demo/src/main/java");
+        gc.setOutputDir(projectPath + "/mybatisPlus/mybatis-demo/src/main/java");
         gc.setAuthor("dqk");
         gc.setOpen(false);
         gc.setBaseResultMap(true);
@@ -86,7 +67,7 @@ public class CodeGenerator2 {
         dsc.setDriverName("com.mysql.jdbc.Driver");
         dsc.setUsername("dev");
         dsc.setPassword("Wingto@2020");*/
-        dsc.setUrl("jdbc:mysql://129.211.65.95:3306/study?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        dsc.setUrl("jdbc:mysql://112.74.47.37:3306/miniapp?useUnicode=true&useSSL=false&characterEncoding=utf8");
         // dsc.setSchemaName("public");
         dsc.setDriverName("com.mysql.jdbc.Driver");
         dsc.setUsername("root");
@@ -99,6 +80,9 @@ public class CodeGenerator2 {
             }
             if (t.startsWith("bigint")) {
                 return DbColumnType.BIG_INTEGER;
+            }
+            if (t.startsWith("tinyint")) {
+                return DbColumnType.INTEGER;
             }
             //其它字段采用默认转换（非mysql数据库可以使用其它默认的数据库转换器）
             return new MySqlTypeConvert().processTypeConvert(globalConfig, fieldType);
@@ -124,7 +108,7 @@ public class CodeGenerator2 {
         PackageConfig pc = new PackageConfig();
 //        String 模块名 = scanner("模块名");
         //pc.setModuleName("test");
-        pc.setParent("com.wingtoweb.apicenter");
+        pc.setParent("com.example.qqminiapp");
         mpg.setPackageInfo(pc);
 
         // 自定义配置
@@ -147,7 +131,7 @@ public class CodeGenerator2 {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath + "/mybatis-demo/src/main/resources/mapper/" + pc.getModuleName()
+                return projectPath + "/mybatisPlus/mybatis-demo/src/main/resources/mapper/" + pc.getModuleName()
                         + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
@@ -192,7 +176,10 @@ public class CodeGenerator2 {
         // 写于父类中的公共字段
         strategy.setSuperEntityColumns("remark");
         //String[] split = scanner("表名，多个英文逗号分割").split(",");
-        strategy.setInclude("userinfo".split(","));
+        strategy.setInclude("t_user".split(","));
+        strategy.setTablePrefix("t_");
+        strategy.setFieldPrefix("f_");
+        strategy.setEntityTableFieldAnnotationEnable(true);
         strategy.setControllerMappingHyphenStyle(true);
         strategy.setTablePrefix(pc.getModuleName() + "_");
 
